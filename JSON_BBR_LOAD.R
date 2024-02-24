043d61a98c938de5ffaee1fae3bf7c342e2f38d4
MathiasLiedtke <mathiasfl13+github@gmail.com>
2024-02-24 16:13:56 +0100

    Ændring

--- JSON_BBR_LOAD.R
+++ JSON_BBR_LOAD.R
@@ -1,2 +1,16 @@
 ### LOAD JSON file (200 gb)
 
+# Library load ----
+library(jsonlite) 
+
+#
+con <- file("/Volumes/Filer_Backup/Speciale/BBR_2023_2024/BBR_2023_2024.json", "r")
+
+# Læs den første linje
+first_line <- readLines(con, n = 1)
+
+# Konverter den første linje til en liste
+data <- fromJSON(first_line)
+
+# Luk filen
+close(con)
