fb439160ede50f78e7aeed3996aad87720b3c81e
MathiasLiedtke <mathiasfl13+github@gmail.com>
2024-03-05 14:35:21 +0100

    Added a script where i clean data from Toke

---
+++ Data_Preparation/Clean_Data_TOKE.R
@@ -0,0 +1,6 @@
+
+# Cleaning data ----
+# This script is made for cleaning the data received from Toke. I imagine to delete columns, outliers, etc. 
+https://www.r-bloggers.com/2021/04/how-to-clean-the-datasets-in-r/
+
+  
